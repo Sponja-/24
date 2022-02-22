a5326b1236750d82a020903801c9b86865d5990b
Sponja- <tomas.spognardi@gmail.com>
2022-02-21 21:31:32 -0300

    Improved expressions

--- expressions.py
+++ expressions.py
@@ -3,43 +3,98 @@ from __future__ import annotations
 from abc import ABC, abstractmethod
 from dataclasses import dataclass
 from numbers import Real
+from typing import Iterable
+
+from multiset import FrozenMultiset
+from more_itertools import nth
 
 
-@dataclass(frozen=True, eq=True)
 class Expression(ABC):
-    args: tuple[Expression]
+    def __init__(self: Expression, *args: Expression | Real) -> None:
+        self.args: Iterable[Expression | Real] = args
 
     @abstractmethod
     def eval(self: Expression) -> Real:
         """Evaluates the Expression"""
         pass
 
+    @abstractmethod
+    def __str__(self: Expression) -> str:
+        pass
 
-@dataclass
-class Constant(ABC):
+    def __hash__(self: Expression):
+        return hash((self.args, type(self)))
+
+
+class Constant(Expression):
+    def __init__(self: Expression, arg: Real) -> None:
+        super().__init__(arg)
+
     def eval(self: Expression) -> Real:
+        return self.value
+
+    def __str__(self: Expression) -> str:
+        return str(self.value)
+
+    @property
+    def value(self: Constant) -> Real:
         return self.args[0]
 
 
-@dataclass
-class Addition(Expression):
+class BinaryOperation(Expression):
+    def __init__(self: BinaryOperation, *args: Expression) -> None:
+        super().__init__(*args)
+
+    def __str__(self: BinaryOperation) -> str:
+        left, right = map(
+            lambda arg: f"({str(arg)})" if type(arg) is not Constant else str(arg),
+            (self.arg1, self.arg2),
+        )
+        return f"{left} {self.symbol} {right}"
+
+    @property
+    def arg1(self: BinaryOperation) -> Expression:
+        return nth(self.args, 0)
+
+    @property
+    def arg2(self: BinaryOperation) -> Expression:
+        return nth(self.args, 1)
+
+    @property
+    @abstractmethod
+    def symbol(self: BinaryOperation) -> str:
+        pass
+
+
+class CommutativeOperation(BinaryOperation):
+    def __init__(self: CommutativeOperation, *args: Expression) -> None:
+        super().__init__(*args)
+        self.args = FrozenMultiset(args)  # Ignores order in hashing
+
+
+class Addition(CommutativeOperation):
+    symbol = "+"
+
     def eval(self: Addition) -> Real:
-        return self.args[0] + self.args[1]
+        return self.arg1.eval() + self.arg2.eval()
 
 
-@dataclass
-class Subtraction(Expression):
+class Subtraction(BinaryOperation):
+    symbol = "-"
+
     def eval(self: Subtraction) -> Real:
-        return self.args[0] - self.args[1]
+        return self.arg1.eval() - self.arg2.eval()
 
 
-@dataclass
-class Multiplication(Expression):
+class Multiplication(CommutativeOperation):
+    symbol = "*"
+
     def eval(self: Multiplication) -> Real:
-        return self.args[0] * self.args[1]
+        return self.arg1.eval() * self.arg2.eval()
 
 
-@dataclass
-class Division(Expression):
+class Division(BinaryOperation):
+    symbol = "/"
+
     def eval(self: Division) -> Real:
-        return self.args[0] / self.args[1]
+        return self.arg1.eval() / self.arg2.eval()
